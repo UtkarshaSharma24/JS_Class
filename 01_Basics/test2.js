9f25681d50bc8f8a660fed38625da23b55cf4872
Utkarsh Sharma <153360041+UtkarshaSharma24@users.noreply.github.com>
2024-10-04 14:45:50 +0000

    change config

---
+++ 01_Basics/test2.js
@@ -0,0 +1,100 @@
+
+/*
+const accountId = 12345;
+let accountEmail = "lucky@example.com";
+var accountPassword = "12345";
+accountCity = "lucy";
+
+accountEmail = "luckyhjl@example.com";  
+accountPassword = "129905";
+accountCity = "delhi";
+
+console.log(accountId);
+
+console.table(accountId, accountEmail, accountPassword, accountCity);
+*/
+
+
+
+
+//=============================================================================================
+
+/*
+"use strict";    //treat all JS code as newer version
+
+// alert(3*3)   //we are using nodejs, not browser
+ 
+console.log(3*3);
+console.log("lucky");
+
+let name = "lucky";
+let age = 23;
+let isLoggedIn = false;
+*/
+
+
+
+//=====================================================================
+
+
+
+//data types--->
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value  or empty 
+//undefined
+//symbol => to define unique 
+//object
+
+
+// console.log(typeof age);
+// console.log(typeof undefined);     //undefined
+// console.log(typeof null);         //object
+
+
+
+//=============================================================================
+
+
+//conversion
+
+// let score = "33abc";
+
+// console.log(typeof score);    //string
+// console.log(typeof(score));   //string
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);  //number which is NaN
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+/*
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)   //boolean  eg. true
+
+let isLoggedIn2 = "lucky";
+let booleanIsLoggedIn2 = boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)   //boolean  eg. true
+*/
+
+//1 => true
+//0 => false
+// "" => false
+// "hitesh" => true
+
+/*
+let someNumber = 34;
+let stringNumber = String(someNumber);
+console.log(stringNumbers);
+console.log(typeof stringNumber); //string
+*/
+
+
